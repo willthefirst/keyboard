8405f53eca47f170b3ee19db129804b3ee671b32
Will Moritz <willthefirst@gmail.com>
2023-07-26 14:01:10 -0700

    Add and unignore hyper-apps

---
+++ hammerspoon/hyper-apps.lua
@@ -0,0 +1,13 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+return {
+    { 'b', 'Arc' },                  -- "B" for "Browser"
+    { 'c', 'Slack' },                -- "C for "Chat"
+    { 'e', 'Visual Studio Code' },   -- "E" for "Editor"
+    { 'm', 'Spotify' },              -- "M" for "Music"
+    { 'f', 'Finder' },               -- "F" for "Finder"
+    { 't', 'Warp' },                 -- "T" for "Terminal"
+}
